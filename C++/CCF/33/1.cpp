ea29505c0370b847b94927bcbed025f16eee1be6
MadMike0204 <1724715245@qq.com>
2024-09-02 20:15:46 +0800

    Updated at 2024/9/2/evening

---
+++ C++/CCF/33/1.cpp
@@ -0,0 +1,80 @@
+#include <iostream> 
+#include <map>
+#include <vector>
+using namespace std;
+
+struct MyDict
+{
+    public:
+        std::map<int,int> frequency;
+        int length;
+        int* article;
+        std::map<string,int> words_frequency;
+    
+        static int articles,words;
+        static std::map<int,int> sum;
+
+        static void Initial(int as,int ws)
+        {
+            articles = as;
+            words = ws;
+            for(int i=1;i<=words;i++)
+            {
+                sum[i] = 0;
+            }
+        }
+
+        MyDict(int l):length(l)
+        {  
+            for(int i=1;i<=words;i++)
+            {
+                frequency[i]=0;
+            }
+            article = new int[length];
+            for(int i=0;i<length;i++)
+            {
+                cin >> article[i];
+                frequency[article[i]]++;
+                sum[article[i]] += 1;
+            }
+        }
+        MyDict(){}      
+};
+
+std::map<int,int> MyDict::sum;
+int MyDict::articles = 0;
+int MyDict::words = 0;
+
+void PrintResult(vector<MyDict>& v)
+{
+    int count;
+    for(int i=1;i<=MyDict::words;i++)
+    {
+        count = 0;
+        for(auto element:v)
+        {
+            if(element.frequency[i])
+            {
+                count++;
+            }
+        }
+        cout << count << " " << MyDict::sum[i] << endl;
+    }
+}
+
+int main(){
+
+    int articles,words,length;
+    cin >> articles >> words;
+    vector<MyDict> Jo;
+    MyDict::Initial(articles,words);
+    while(articles--)
+    {
+        cin >> length;
+        MyDict temp(length);
+        Jo.push_back(temp);
+    }
+    PrintResult(Jo);
+
+    return 0;
+}
